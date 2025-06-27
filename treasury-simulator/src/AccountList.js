fdcb7e11b801bfa560c97d7f7e028f75469fc65e
fidelis182 <fedymueni18gmail.com>
2025-06-27 12:14:09 +0300

    styled my acountList table

--- treasury-simulator/src/AccountList.js
+++ treasury-simulator/src/AccountList.js
@@ -1,22 +1,43 @@
+import { useEffect, useState } from "react";
+
 export default function AccountList() {
+  const [accounts, setAccount] = useState("");
+  useEffect(() => {
+    // to get data from the API
+    fetch("http://localhost:5000/accounts")
+      // promise
+      .then((res) => res.json())
+      // to update data
+      .then((data) => setAccount(data))
+      .catch((err) => console.log(err.message));
+  }, []);
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Account_Name</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>ID</td>
-            <td>Account_Name</td>
-            <td>Cuurency</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className=".container">
+      <div className=".table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Account_name</th>
+              <th>Currency</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {accounts &&
+              accounts.map((item) => (
+                <tr key={item.id}>
+                  {" "}
+                  {/* ✅ Add key here */}
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.currency}</td>
+                  <td>{item.balance}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- treasury-simulator/src/App.js
+++ treasury-simulator/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import React from "react";
+import AccountList from "./AccountList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return(
-    
-    
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AccountList />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
