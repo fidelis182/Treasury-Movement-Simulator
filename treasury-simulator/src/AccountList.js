c38ab2c6b206f5839223d5d596a2ec60c5ccb42c
fidelis182 <fedymueni18gmail.com>
2025-06-27 11:45:54 +0300

    created neccessary files and dependancies

---
+++ treasury-simulator/src/AccountList.js
@@ -0,0 +1,22 @@
+export default function AccountList() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Account_Name</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>Account_Name</td>
+            <td>Cuurency</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ treasury-simulator/src/TransactionLog.js
@@ -0,0 +1,21 @@
+export default function TransactionLog() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="account_from">Account_From:</label>
+        <input type="text" id="account_from" name="account_from" />
+        <label htmlFor="account_To">Account_To:</label>
+        <input type="text" id="account_To" name="account_To" />
+        <label htmlFor="amount">Amount:</label>
+        <input type="text" id="amount" name="amount" />
+        <label htmlFor="currency">Currency:</label>
+        <input type="text" id="currency" name="currency" />
+        <label htmlFor="date">Date:</label>
+        <input type="text" id="date" name="date" />
+        <label htmlFor="comment">Comment:</label>
+        <input type="text" id="comment" name="comment" />
+        <button>Transfer</button>
+      </form>
+    </div>
+  );
+}
