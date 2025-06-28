0e2af5f226233d1804578c75ccefa3d3cf04b8c9
fidelis182 <fedymueni18gmail.com>
2025-06-28 10:59:33 +0300

    hot fix on sidebar

--- treasury-simulator/src/Data/Data.js
+++ treasury-simulator/src/Data/Data.js
@@ -1,31 +1,14 @@
-// Sidebar import
-import {
-  UilEstate,
-  UilClipboardAlt,
-  UilUsersAlt,
-  UilChart,
-  UilPackage,
-} from "@iconscout/react-unicons";
+import { UilEstate, UilClipboardAlt } from "@iconscout/react-unicons";
 
 export const SidebarData = [
   {
     icon: UilEstate,
-    heading: "Dashboard",
+    heading: "Account View",
+    path: "/",
   },
   {
     icon: UilClipboardAlt,
-    heading: "Orders",
-  },
-  {
-    icon: UilUsersAlt,
-    heading: "Customers",
-  },
-  {
-    icon: UilPackage,
-    heading: "Products",
-  },
-  {
-    icon: UilChart,
-    heading: "Analytics",
+    heading: "Transaction Log",
+    path: "/transactions",
   },
 ];
--- treasury-simulator/src/components/Sidebar/SideBar.jsx
+++ treasury-simulator/src/components/Sidebar/SideBar.jsx
@@ -1,31 +1,27 @@
 import { SidebarData } from "../../Data/Data";
 import "./sidebar.css";
 import { UilSignOutAlt } from "@iconscout/react-unicons";
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
-  // useState hook to have onely the first item selected
-  const [selected, setselected] = useState(0);
   return (
     <div className="Sidebar">
-      {/* Menu
-       */}
       <div className="menu">
-        {SidebarData.map((item, index) => {
-          return (
-            <div
-              // sets the first element as the selected
-              className={selected === index ? "menuItem active" : "menuItem"}
-              // this one set the clicked one as the selected
-              onClick={() => setselected(index)}
-              key={index}
-            >
-              <item.icon />
-              <span>{item.heading}</span>
-            </div>
-          );
-        })}
-        <div className="menuItem">
+        {SidebarData.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className={({ isActive }) =>
+              isActive ? "menuItem active" : "menuItem"
+            }
+          >
+            <item.icon />
+            <span>{item.heading}</span>
+          </NavLink>
+        ))}
+        <div className="menuItem logout">
           <UilSignOutAlt />
+          <span>Logout</span>
         </div>
       </div>
     </div>
